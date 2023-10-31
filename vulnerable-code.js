5b192cdefa76988ee1ef612167901d06e8fb8f67
Alain Tran-Dang <alain.tran-dang@avanade.com>
2023-10-31 10:01:56 -0400

    fixed vulnerable code

--- vulnerable-code.js
+++ vulnerable-code.js
@@ -1,9 +1,5 @@
 function endsWith(x, y) {
 
-    if (x === null || x === undefined || y === null || y === undefined) {
-        throw new Error('Invalid input');
-    }
-    
     let index = x.lastIndexOf(y);
-    return x.lastIndexOf(y) === x.length - y.length;
+    return index !== -1 && index === x.length - y.length;
 }
